495d64da62d0ea92802037f5b67fedf8703846aa
o614 <49578959+o614@users.noreply.github.com>
2026-01-10 11:29:38 +0800

    Update handlers.js

--- api/handlers.js
+++ api/handlers.js
@@ -170,10 +170,10 @@ function handleRegionSwitch(regionName) {
   const cnUrl = `https://itunes.apple.com/WebObjects/MZStore.woa/wa/resetAndRedirect?dsf=${cnDsf}&cc=${cnCode}&url=${encodeURIComponent(redirectPath)}`;
   const cnRawUrl = `itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/resetAndRedirect?dsf=${cnDsf}&cc=${cnCode}`;
 
-  return `注意！仅可浏览，需对应账号才能下载\n出现“无法连接”后等待片刻将自动跳转\n\n` +
+  return `注意！仅浏览，需对应账号才能下载\n出现“无法连接”等待片刻自动跳转\n\n` +
          `› <a href="${fullUrl}">点击切换至【${regionName}】 的 App Store</a>\n\n` +
-         `› 点此切换至 <a href="${cnUrl}">【中国大陆】</a> 的 App Store\n\n` +
-         `备用（请长按链接复制到 Safari 打开）\n\n` +
+         `› 点此切换至 <a href="${cnUrl}">【国区】</a> 的 App Store\n\n\n` +
+         `备用（请长按并复制到 Safari 打开）\n` +
          `${regionName}：\n<a href="weixin://">${rawUrl}</a>\n\n` +
          `中国：\n<a href="weixin://">${cnRawUrl}</a>`;
 }
