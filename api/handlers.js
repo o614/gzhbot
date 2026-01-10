f2bee0c722d8fc9fe13eb99f9b1d8701eca5be23
o614 <49578959+o614@users.noreply.github.com>
2026-01-10 11:38:04 +0800

    Update handlers.js

--- api/handlers.js
+++ api/handlers.js
@@ -173,7 +173,7 @@ function handleRegionSwitch(regionName) {
   return `注意！仅浏览，需对应账号才能下载\n*出现“无法连接”等待片刻自动跳转\n\n` +
          `› <a href="${fullUrl}">点击切换至【${regionName}】 的 App Store</a>\n\n` +
          `› 点此切换至 <a href="${cnUrl}">【国区】</a> 的 App Store\n\n\n` +
-         `*备用（请长按并复制到 Safari 打开）\n\n` +
+         `*iOS 26（请长按复制到 Safari 打开）\n\n` +
          `${regionName}：\n<a href="weixin://">${rawUrl}</a>\n\n` +
          `中国：\n<a href="weixin://">${cnRawUrl}</a>`;
 }
